724b1bf0249c891f9ea235ce0b1f1cdf08ad454d
Ioan <ioan@htmlthis.com>
2019-02-10 00:55:01 -0500

    modifies data structure, adds message parsing logic

--- DistanceVector.py
+++ DistanceVector.py
@@ -17,6 +17,8 @@
         											
 from Node import *
 from helpers import *
+import re
+import time
 
 class DistanceVector(Node):
     
@@ -28,7 +30,8 @@ class DistanceVector(Node):
         super(DistanceVector, self).__init__(name, topolink, outgoing_links, incoming_links)
         #TODO: Create any necessary data structure(s) to contain the Node's internal state / distance vector data    
         self.in_state = dict()
-        self.in_state["vector"]=["{name}{distance}".format(name=self.name, distance=0)]
+        self.in_state["nodes"]={"{}".format(self.name):0}
+        self.in_state["vector"]=set(["{name}{distance}".format(name=self.name, distance=0)])
         self.in_state["is_updated"]=True
 
     def send_initial_messages(self):
@@ -42,8 +45,10 @@ class DistanceVector(Node):
 
         # TODO - Each node needs to build a message and send it to each of its neighbors
         # HINT: Take a look at the skeleton methods provided for you in Node.py
+        #print("node {} incoming {}".format(self.name, [n.name for n in self.incoming_links]))
         for node in self.incoming_links:
-            message = (self.name, "{node_name}{distance_cost}".format(node_name=self.name, distance_cost=node.weight))
+            message = (self.name, "{node_name}{distance_cost}".format(node_name=self.name, distance_cost=0))
+            print("Initial send from {} to {} {}".format(self.name, node.name, message))
             self.send_msg(message, node.name)
 
 
@@ -51,25 +56,42 @@ class DistanceVector(Node):
         ''' This is run continuously (repeatedly) during the simulation. DV
         messages from other nodes are received here, processed, and any new DV
         messages that need to be sent to other nodes as a result are sent. '''
-
         # Implement the Bellman-Ford algorithm here.  It must accomplish two tasks below:
-        # TODO 1. Process queued messages       
+        # TODO 1. Process queued messages 
+        print self.name, self.in_state, self.messages
         for msg in self.messages:            
             received_from, vector = msg
-            self.in_state['vector'].append(vector)
-
-        #adding a node for the 1st time
-       
+            for vector_bit in vector.split(','):
+                node, weight, _ = re.split(r'(-?[0-9]+)', vector_bit)
+                if node in [n.name for n in self.outgoing_links]:
+                    weight = int(self.get_outgoing_neighbor_weight(node))
+                    if weight != self.in_state["nodes"].get(node, float("Inf")):
+                        self.in_state["nodes"][node] = int(weight)
+                        self.in_state['is_updated'] = True
+                elif node == self.name:
+                    self.in_state['is_updated'] = False
+                    continue # we don't update our current node's distance
+                else:
+                    weight = int(weight)
+                    if weight != self.in_state['nodes'].get(node, float("Inf")):
+                        self.in_state["nodes"][node] = weight + self.in_state["nodes"][received_from]
+                        self.in_state['is_updated'] = True
+                        print "switch updated on ", self.in_state["nodes"][node]
+   
         print self.name, self.in_state
-
         # Empty queue
         self.messages = []
 
         # TODO 2. Send neighbors updated distances               
         if self.in_state['is_updated']:
-           pass 
+            for node in self.incoming_links:
+               message = (self.name, ','.join(["{}{}".format(n, self.in_state["nodes"][n]) for n in self.in_state["nodes"]]))
+               print node.name, message
+               self.send_msg(message, node.name)
+
         # set to  off updated switch
-        self.in_state['is_updated']=False
+        self.in_state['is_updated'] = False
+        raw_input()
 
 
     def log_distances(self):
@@ -85,5 +107,5 @@ class DistanceVector(Node):
         
         # TODO: Use the provided helper function add_entry() to accomplish this task (see helpers.py).
         # An example call that which prints the format example text above (hardcoded) is provided. 
-        add_entry(self.name, ','.join(self.in_state['vector']))
+        add_entry(self.name, ','.join(["{}{}".format(n, self.in_state["nodes"][n]) for n in self.in_state["nodes"]]))
         #add_entry("A", "A0,B1,C2")        
